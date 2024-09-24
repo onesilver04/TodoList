a44ce1603bd254540d063453cb4c9843d9af499c
onesilver04 <eunjjung123@sookmyung.ac.kr>
2024-09-25 00:35:30 +0900

    create delte button

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,32 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
 import "./TodoItem.css";
+import deleteIcon from "./delete.png"; // 이미지 파일 경로 설정
 
-class TodoItem extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return this.props.checked !== nextProps.checked;
-  }
-
-  render() {
-    const { text, checked, id, onToggle, onRemove } = this.props;
-
-    return (
-      <div className="todo-item" onClick={() => onToggle(id)}>
-        <div
-          className="remove"
-          onClick={(e) => {
-            e.stopPropagation(); // onToggle 이 실행되지 않도록 함
-            onRemove(id);
-          }}
-        >
-          &times;
-        </div>
-        <div className={`todo-text ${checked && "checked"}`}>
-          <div>{text}</div>
-        </div>
-        {checked && <div className="check-mark">&#x2713;</div>}
+const TodoItem = ({ text, checked, id, onToggle, onRemove }) => {
+  return (
+    <div className="todo-item" onClick={() => onToggle(id)}>
+      <div
+        className="remove"
+        onClick={(e) => {
+          e.stopPropagation(); // onToggle 실행되지 않도록 함
+          onRemove(id); // Todo 항목 삭제
+        }}
+      >
+        &times;
       </div>
-    );
-  }
-}
+      <div className={`todo-text ${checked && "checked"}`}>
+        <div>{text}</div>
+      </div>
+      {checked && <div className="check-mark">&#x2713;</div>}
+      {/* 삭제 이미지 버튼 */}
+      <img
+        src={deleteIcon}
+        alt="삭제"
+        className="delete-button"
+        onClick={(e) => {
+          e.stopPropagation(); // onToggle 실행 방지
+          onRemove(id); // Todo 항목 삭제
+        }}
+      />
+    </div>
+  );
+};
 
 export default TodoItem;
